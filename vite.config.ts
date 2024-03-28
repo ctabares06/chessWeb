ffa2fc364798c6c324c804cce0e3d140b0309ae7
ctabares06 <ctr9120@gmail.com>
2024-03-28 16:20:15 -0500

    replace base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/chessWeb/',
   plugins: [react()],
   css: {
     modules: {
